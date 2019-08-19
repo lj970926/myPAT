2d1335fb0647c188797f297d751da3ca9a609b52
lijin <1783973490@qq.com>
2019-08-19 13:06:47 +0800

    UPDATE 1052

--- 1052/main.cpp
+++ 1052/main.cpp
@@ -2,29 +2,35 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-struct Node {
-    int p,k;
-    Node(int p_, int k_):p(p_), k(k_){};
+struct Node{
+    int addr, key;
 };
+vector<Node> ori, l;
 
-bool cmp(Node &a, Node &b){
-    return a.k < b.k;
+bool cmp(Node a, Node b){
+    return a.key < b.key;
 }
-vector<Node> l;
-int main()
-{
-    int n, head;
-    scanf("%d %d", &n, &head);
+
+int main(){
+    int n, h;
+    ori.resize(100000);
+    scanf("%d %d", &n, &h);
     for (int i = 0; i < n; i++){
-        int p, k, ne;
-        scanf("%d %d %d", &p, &k, &ne);
-        l.push_back(Node(p,k));
+        int a, k, n;
+        scanf("%d %d %d", &a, &k, &n);
+        ori[a].key = k;
+        ori[a].addr = n;
     }
-    printf("%d ", n);
+    for (int p = h; p != -1; p = ori[p].addr){
+        Node t;
+        t.addr = p;
+        t.key = ori[p].key;
+        l.push_back(t);
+    }
     sort(l.begin(), l.end(), cmp);
-    for (int i = 0; i < l.size();i++){
-        printf("%05d\n%05d", l[i].p, l[i].p);
-        printf(" %d ", l[i].k);
+    printf("%d ", l.size());
+    for (int i = 0; i < l.size(); i++){
+        printf("%05d\n%05d %d ", l[i].addr, l[i].addr, l[i].key);
     }
     printf("-1");
     return 0;
