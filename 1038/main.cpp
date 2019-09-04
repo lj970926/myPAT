6bd2fc2b8aeb075d1fef458506182b1eec0bb6ac
lijin <1783973490@qq.com>
2019-09-04 14:38:33 +0800

    improve 1038

--- 1038/main.cpp
+++ 1038/main.cpp
@@ -1,59 +1,33 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include <algorithm>
+#include <vector>
 using namespace std;
+vector<string> seg;
 
-bool cmp(string &a, string &b){
-    int i = 0;
-    while (i < a.size()&&i < b.size()&&a[i] == b[i]) i++;
-    if (i < a.size() && i < b.size()) return a[i] < b[i];
-    if (i < a.size()){
-        int p = 0;
-        while (a[i] == a[p]&&i < a.size()){
-            i++;p++;
-        }
-        return a[i] < a[p];
-    }
-    if (i < b.size()){
-        int p = 0;
-        while (b[i] == b[p]&&i < b.size()){
-            i++;p++;
-        }
-        return b[i] > b[p];
-    }
-    return a[i] < b[i];
-
+bool cmp(string a, string b){
+    string s1 = a + b, s2 = b + a;
+    return s1 < s2;
 }
 
-int main()
-{
+int main(){
     int n;
-    scanf("%d" ,&n);
-    vector<string> seg;
-    for (int i = 0;i < n;i++){
-        string temp;
-        cin >> temp;
-        bool result = false;
-        for (int i = 0;i < temp.size();i++)
-            if (temp[i] != '0'){
-                result = true;
-                break;
-            }
-        if (result)
-            seg.push_back(temp);
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++){
+        string s;
+        cin >> s;
+        if (stoi(s) != 0)
+            seg.push_back(s);
     }
     if (seg.size() == 0){
-        cout << 0;
+        printf("%d", 0);
         return 0;
     }
-    sort(seg.begin(),seg.end(),cmp);
-    int pos = seg[0].find_first_not_of("0");
-    if (pos < seg[0].size())
-        seg[0] = seg[0].substr(pos);
-    else
-        seg[0] = "";
-    for (int i = 0; i < seg.size();i++)
-        cout << seg[i];
+    sort(seg.begin(),  seg.end(), cmp);
+    seg[0] = seg[0].substr(seg[0].find_first_not_of("0"));
+    for (int i = 0; i < seg.size(); i++){
+        printf("%s", seg[i].c_str());
+    }
+    printf("\n");
     return 0;
 }
