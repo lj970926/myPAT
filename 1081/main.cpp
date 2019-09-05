9567bbde50b4a33cdc43c41dc9a53fe96b3df5c5
lijin <1783973490@qq.com>
2019-09-05 19:31:17 +0800

    improve 1081

--- 1081/main.cpp
+++ 1081/main.cpp
@@ -2,41 +2,52 @@
 #include <algorithm>
 using namespace std;
 struct Fraction{
-    long long up, down;
+    long long up = 0, down = 1;
 };
 
 long long gcd(long long a, long long b){
-    return b == 0? a : gcd(b, a % b);
+    return b == 0 ? a : gcd(b, a % b);
+}
+
+Fraction reduction(Fraction a){
+    long long d = gcd(abs(a.up) , abs(a.down));
+    if (a.down < 0){
+        a.down = - a.down;
+        a.up = - a.up;
+    }
+    a.up /= d;
+    a.down /= d;
+    return a;
 }
 
 Fraction add(Fraction a, Fraction b){
-    Fraction result;
-    result.down = a.down * b.down;
-    result.up = a.up * b.down + b.up * a.down;
-    int d = gcd(abs(result.up), abs(result.down));
-    result.up /= d;
-    result.down /= d;
-    return result;
+    Fraction ans;
+    ans.down = a.down * b.down;
+    ans.up = a.up * b.down + a.down * b.up;
+    return reduction(ans);
 }
 
-int main()
-{
+void show(Fraction a){
+    if (a.down == 1){
+        printf("%lld\n", a.up);
+    }
+    else if (abs(a.up) > a.down){
+        printf("%lld %lld/%lld\n", a.up / a.down, abs(a.up % a.down), a.down);
+    }
+    else{
+        printf("%lld/%lld\n", a.up, a.down);
+    }
+}
+
+int main(){
     int n;
     scanf("%d", &n);
     Fraction sum;
-    sum.up = 0;
-    sum.down = 1;
     for (int i = 0; i < n; i++){
-        Fraction a;
-        scanf("%lld/%lld", &a.up, &a.down);
-        sum = add(sum, a);
+        Fraction tmp;
+        scanf("%lld/%lld", &tmp.up, &tmp.down);
+        sum = add(sum, tmp);
     }
-    if (sum.down == 1)
-        printf("%lld", sum.up);
-    else if (abs(sum.up) > sum.down){
-        printf("%lld %lld/%lld", sum.up / sum.down, abs(sum.up) % sum.down, sum.down);
-    }
-    else
-        printf("%lld/%lld", sum.up, sum.down);
+    show(sum);
     return 0;
 }
